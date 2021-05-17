9f5ff1415a43fe17a885c52861ee939eb53b94cd
nappie <nappie_xd@hotmail.com>
2021-05-17 10:58:41 +0200

    third commit, storefront assignments done

--- NpStTemplate_02/src/Resources/app/storefront/src/main.js
+++
@@ -1,3 +0,0 @@
-import SearchBarPlugin from './script/search-bar.plugin'
-
-window.PluginManager.register('SearchBar', SearchBarPlugin, '.header-row')
--- NpStTemplate_02/src/Resources/app/storefront/src/script/search-bar.plugin.js
+++
@@ -1,9 +0,0 @@
-import Plugin from 'src/plugin-system/plugin.class'
-import DomAccess from 'src/helper/dom-access.helper'
-
-export default class SearchBarPlugin extends Plugin {
-    init() {
-        console.info("Plugin Search Bar loaded")
-    }
-
-}
